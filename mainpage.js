4ae553ae0705adc98180da8c4b8b14a364ee431c
ssups <107898015+ssups@users.noreply.github.com>
2022-06-27 18:30:39 +0900

    20220627

--- mainpage.js
+++ mainpage.js
@@ -1,12 +1,12 @@
 //스크롤 버튼
 window.onscroll = function(){
     let main = document.querySelector('.main div:first-child') 
-    if ( main.getBoundingClientRect().top < 150){
+    if ( main.getBoundingClientRect().top < 120){
        document.querySelector('.scrollContents').style.zIndex = '1' 
        scrollBtn.style.opacity = '1'
-       scrollBtn.style.top = '605px'
+       scrollBtn.style.top = '400px'
        setTimeout(()=>{
-        scrollBtn.style.top = '540px'
+        scrollBtn.style.top = '340px'
        },500)
         
        
@@ -15,7 +15,7 @@ window.onscroll = function(){
         scrollBtn.style.opacity = '0'
         document.querySelector('.scrollContents').style.zIndex = '-999' 
         
-        scrollBtn.style.top = '525px'
+        scrollBtn.style.top = '300px'
         
     }
 }
@@ -28,45 +28,47 @@ scrollBtn.onclick = function(){
 
 //사이드메뉴 
 let leftMenuCount = document.querySelector('.leftMenu').childElementCount
-console.log(leftMenuCount)
-console.log(document.querySelector(`.leftMenudiv:nth-child(${2})`))
-allDiv=document.querySelectorAll('.leftMenudiv')
-console.log(allDiv[0])
-console.log(allDiv[1])
-console.log(allDiv[2])
+allLeftMenuDiv=document.querySelectorAll('.leftMenudiv')
 
 for (let i = 1; i<=leftMenuCount; i++){
     document.querySelector(`.leftMenu li:nth-child(${i})`).onmouseenter = function(){
         for(let i = 1; i<=leftMenuCount; i++){
             console.log(leftMenuCount)
             console.log(i)
-            if(allDiv[i-1].classList.contains('start')){
-                allDiv[i-1].classList.remove('start')
+            if(allLeftMenuDiv[i-1].classList.contains('start')){
+                allLeftMenuDiv[i-1].classList.remove('start')
             }
-            if(allDiv[i-1].classList.contains('active')){
-                allDiv[i-1].classList.remove('active')
+            if(allLeftMenuDiv[i-1].classList.contains('active')){
+                allLeftMenuDiv[i-1].classList.remove('active')
             }
         }   
-        allDiv[i-1].classList.add('start')
+        allLeftMenuDiv[i-1].classList.add('start')
         setTimeout(() => {
-            allDiv[i-1].classList.add('active')
-        }, );
+            allLeftMenuDiv[i-1].classList.add('active')
+        }, 100);
     }
-    // document.querySelectorAll(`.leftMenu li:nth-child(${i})`).onmouseleave = function(){
-    //     for(let i = 1; i<=leftMenuCount; i++){
-    //         console.log('나감')
-    //         if(document.querySelector(`.leftMenu li:nth-child(${i})`).classList.contains('start')){
-    //             document.querySelector(`.leftMenu li:nth-child(${i})`).classList.remove('start')
-    //         }
-    //         if(document.querySelector(`.leftMenu li:nth-child(${i})`).classList.contains('active')){
-    //             document.querySelector(`.leftMenu li:nth-child(${i})`).classList.remove('active')
-    //         }
-    //     }   
-    // }
 }
 
+document.querySelector(`.leftMenu`).onmouseleave = function(){
+    for(let i = 1; i<=leftMenuCount; i++){
+        console.log('나감')
+        if(allLeftMenuDiv[i-1].classList.contains('start')){
+            allLeftMenuDiv[i-1].classList.remove('start')
+        }
+        if(allLeftMenuDiv[i-1].classList.contains('active')){
+            allLeftMenuDiv[i-1].classList.remove('active')
+        }
+    }   
+}
 // document.querySelectorAll('.leftMenu li').forEach((e,index)=>{
 //     console.log(e)
 //     console.log(index)
 // })
+
+// let DivCountFunc = function(super_id){
+
+// }
+
+
+
 
