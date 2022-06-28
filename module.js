783f2bef75b918d22b31cdebe80edab60582ce32
ssups <107898015+ssups@users.noreply.github.com>
2022-06-28 18:18:13 +0900

    new

--- module.js
+++ module.js
@@ -1,16 +1,12 @@
 let DivGnb=(
     function(){
         function DivGnb(super_id){
-            console.log(super_id)
             this.init(super_id)
         }
         DivGnb.prototype.init = function(super_id){
-            console.log(super_id)
             let id = super_id
-            console.log(id)
             if(id ==="" || id === null){return}
             let temp = document.createElement('div')
-            console.log(temp)
             id.appendChild(temp);
          }
          return DivGnb
