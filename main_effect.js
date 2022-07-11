6c3fd0d7bb9887c7ef12245f2283b1fe51654c1b
ssups <shalstlqruki@gmail.com>
2022-07-11 12:12:44 +0900

    공백

--- main_effect.js
+++ main_effect.js
@@ -1,30 +1,26 @@
 //=============홈페이지 이펙트=====================================
 //스크롤 리모컨 버튼
-window.onscroll = function(){
-    let main = document.querySelector('.main div:first-child') 
-    if ( main.getBoundingClientRect().top < 120){
-        document.querySelector('.scrollContents').style.zIndex = '1' 
-        scrollBtn.style.opacity = '1'
-        scrollBtn.style.top = '400px'
-        setTimeout(()=>{
-            scrollBtn.style.top = '340px'
-        },500)
-        
-        
-    }
-    else{
-        scrollBtn.style.opacity = '0'
-        document.querySelector('.scrollContents').style.zIndex = '-999' 
-        
-        scrollBtn.style.top = '300px'
-        
-    }
-}
+window.onscroll = function () {
+  let main = document.querySelector(".main div:first-child");
+  if (main.getBoundingClientRect().top < 120) {
+    document.querySelector(".scrollContents").style.zIndex = "1";
+    scrollBtn.style.opacity = "1";
+    scrollBtn.style.top = "400px";
+    setTimeout(() => {
+      scrollBtn.style.top = "340px";
+    }, 500);
+  } else {
+    scrollBtn.style.opacity = "0";
+    document.querySelector(".scrollContents").style.zIndex = "-999";
 
-scrollBtn.onclick = function(e){
-    e.preventDefault() //a태그가 비어있으면 새로고침되는거 방지
-    window.scrollTo({top:0, behavior:"smooth"})
-}
+    scrollBtn.style.top = "300px";
+  }
+};
+
+scrollBtn.onclick = function (e) {
+  e.preventDefault(); //a태그가 비어있으면 새로고침되는거 방지
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
 //왼쪽 메뉴바 저승으로
 // //왼쪽 메뉴바 효과
@@ -40,7 +36,7 @@ scrollBtn.onclick = function(e){
 //             if(allLeftMenuDiv[i-1].classList.contains('active')){
 //                 allLeftMenuDiv[i-1].classList.remove('active')
 //             }
-//         }   
+//         }
 //         allLeftMenuDiv[i-1].classList.add('start')
 //         setTimeout(() => {
 //             allLeftMenuDiv[i-1].classList.add('active')
@@ -56,7 +52,7 @@ scrollBtn.onclick = function(e){
 //         if(allLeftMenuDiv[i-1].classList.contains('active')){
 //             allLeftMenuDiv[i-1].classList.remove('active')
 //         }
-//     }   
+//     }
 // }
 // //======================================================================
 
@@ -68,5 +64,3 @@ scrollBtn.onclick = function(e){
 // MenuHome.onclick = function(){
 //     location.replace("../mainpage/mainpage.html")
 // }
-
-
--- mainpage/scrollevent.js
+++ mainpage/scrollevent.js
@@ -1,149 +1,140 @@
 //스크롤 이벤트
 
-
 // //애니메이션 넣어주는거 안먹음
 // let webkitInFun = function(queryName,webkitName,time){
 //     queryName.style.webkitAnimationName = webkitName
 //     queryName.style.webkitAnimationDuration = time
 // }
 
-
 //스크롤시 옆으로 쑥 나오는거
-
 
 let distinguishScroll = 0;
 //스크롤 이벤트용
-for(let i =0; i<postWritesArr.length;i++){
-    toStopDrawFun.push("")
+for (let i = 0; i < postWritesArr.length; i++) {
+  toStopDrawFun.push("");
 }
 
-window.onscroll = function(){
-    let distinguishScrollUpDown;
-    //스크롤 업다운 구별
-    if(distinguishScroll<window.scrollY){
-        distinguishScrollUpDown = "Down"
+window.onscroll = function () {
+  let distinguishScrollUpDown;
+  //스크롤 업다운 구별
+  if (distinguishScroll < window.scrollY) {
+    distinguishScrollUpDown = "Down";
+  } else {
+    distinguishScrollUpDown = "Up";
+  }
+  distinguishScroll = window.scrollY;
+  //스크롤 업다운 구별
+
+  for (let i = 0; i < postWritesArr.length; i++) {
+    // //바퀴 그려주기
+    // let contentTire = postWritesArr[i].querySelector('.contentTire');
+    // let contentTireCtx =contentTire.getContext('2d')
+    // let degree = 0;
+
+    // let radian = function(degree){
+    //     return degree*(Math.PI/180);
+    // }
+
+    // let contentTireDrawFun = function(stop){
+    //     if(stop == "stop"){
+    //         return
+    //     }
+    //     let img = new Image();
+    //     img.src = "../images/contentTire.png"
+    //     img.onload = function(){
+    //         contentTire.style.display = "block"
+    //     //첫번째 바퀴
+    //     contentTireCtx.clearRect(0,0,contentTire.width,contentTire.height);
+    //     contentTireCtx.save();
+    //     contentTireCtx.translate(85,95);
+    //     contentTireCtx.rotate(-radian(degree))
+    //     contentTireCtx.drawImage(img,-25,-25,50,50);
+    //     contentTireCtx.restore();
+    //     //두번째 바퀴
+    //     contentTireCtx.save();
+    //     contentTireCtx.translate(225,95);
+    //     contentTireCtx.rotate(-radian(degree))
+    //     contentTireCtx.drawImage(img,-25,-25,50,50);
+    //     contentTireCtx.restore();
+
+    //     degree += 1;
+    //     if(degree == 180){
+    //         contentTire.style.display = "none"
+    //         return
+    //     }
+
+    //     requestAnimationFrame(contentTireDrawFun);
+
+    //     }
+    // }
+
+    // //transporter 그리기
+    // let contentTransPorter = postWritesArr[i].querySelector('.contentTransPorter');
+    // let contentTransPorterCtx = contentTransPorter.getContext('2d');
+
+    //스크롤 이벤트
+    let postWritesArrTop = postWritesArr[i].getBoundingClientRect().top;
+    let postWritesArrBottom = postWritesArr[i].getBoundingClientRect().bottom;
+    //스크롤 올릴때
+    if (distinguishScrollUpDown == "Up") {
+      //효과 off
+      if (window.innerHeight - postWritesArrTop < 50) {
+        postWritesArr[i].style.webkitAnimationName = "";
+        postWriteShadow[i].style.webkitAnimationName = "";
+        postWritesArr[i].style.webkitAnimationDuration = "";
+        postWriteShadow[i].style.webkitAnimationDuration = "";
+        postWritesArr[i].style.opacity = 0;
+        // contentTireDrawFun("stop")
+        // toStopDrawFun[i] = 0;
+      }
+      // //효과 on
+      // else if(postWritesArrTop>15){
+
+      //     postWritesArr[i].style.webkitAnimationName = "fadein"
+      //     postWritesArr[i].style.webkitAnimationDuration = "3s"
+      //     postWriteShadow[i].style.webkitAnimationName = "fadeinShadow"
+      //     postWriteShadow[i].style.webkitAnimationDuration = "3s"
+      //     postWritesArr[i].style.opacity = 1;
+      //     if(toStopDrawFun[i] ==1){
+      //         return
+      //     }
+      //     contentTireDrawFun();
+      //     toStopDrawFun[i] = 1;
+      // }
     }
-    else{
-        distinguishScrollUpDown = "Up"
+
+    //스크롤 내릴때
+    else {
+      //효과 off
+      // if(postWritesArrBottom<150){
+      //     postWritesArr[i].style.webkitAnimationName = ""
+      //     postWriteShadow[i].style.webkitAnimationName = ""
+      //     postWritesArr[i].style.opacity = 0;
+      //     toStopDrawFun[i] = 0;
+      // }
+      // //효과 on
+      // else
+      if (window.innerHeight - postWritesArrBottom > -100) {
+        return;
+      }
+      if (window.innerHeight - postWritesArrTop > 50) {
+        postWritesArr[i].style.webkitAnimationName = "fadein";
+        postWritesArr[i].style.webkitAnimationDuration = "3s";
+        postWriteShadow[i].style.webkitAnimationName = "fadeinShadow";
+        postWriteShadow[i].style.webkitAnimationDuration = "3s";
+        postWritesArr[i].style.opacity = 1;
+        if (toStopDrawFun[i] == 1) {
+          return;
+        }
+        // contentTireDrawFun();
+        // toStopDrawFun[i] = 1;
+      } else {
+        postWritesArr[i].style.webkitAnimationName = "";
+        postWriteShadow[i].style.webkitAnimationName = "";
+        postWritesArr[i].style.webkitAnimationDuration = "";
+        postWriteShadow[i].style.webkitAnimationDuration = "";
+        postWritesArr[i].style.opacity = 0;
+      }
     }
-    distinguishScroll = window.scrollY;
-    //스크롤 업다운 구별
-
-    for(let i = 0; i<postWritesArr.length;i++){
-        // //바퀴 그려주기
-        // let contentTire = postWritesArr[i].querySelector('.contentTire');
-        // let contentTireCtx =contentTire.getContext('2d')
-        // let degree = 0;
-        
-        // let radian = function(degree){
-        //     return degree*(Math.PI/180);
-        // }
-        
-        // let contentTireDrawFun = function(stop){
-        //     if(stop == "stop"){
-        //         return
-        //     }
-        //     let img = new Image();
-        //     img.src = "../images/contentTire.png"
-        //     img.onload = function(){
-        //         contentTire.style.display = "block"
-        //     //첫번째 바퀴
-        //     contentTireCtx.clearRect(0,0,contentTire.width,contentTire.height);
-        //     contentTireCtx.save();
-        //     contentTireCtx.translate(85,95);
-        //     contentTireCtx.rotate(-radian(degree))
-        //     contentTireCtx.drawImage(img,-25,-25,50,50);
-        //     contentTireCtx.restore();
-        //     //두번째 바퀴
-        //     contentTireCtx.save();
-        //     contentTireCtx.translate(225,95);
-        //     contentTireCtx.rotate(-radian(degree))
-        //     contentTireCtx.drawImage(img,-25,-25,50,50);
-        //     contentTireCtx.restore();
-            
-        //     degree += 1;
-        //     if(degree == 180){
-        //         contentTire.style.display = "none"
-        //         return
-        //     }
-            
-        //     requestAnimationFrame(contentTireDrawFun);
-            
-        //     }
-        // }
-
-        // //transporter 그리기
-        // let contentTransPorter = postWritesArr[i].querySelector('.contentTransPorter');
-        // let contentTransPorterCtx = contentTransPorter.getContext('2d');
-        
-
-
-
-
-        //스크롤 이벤트
-        let postWritesArrTop = postWritesArr[i].getBoundingClientRect().top;
-        let postWritesArrBottom = postWritesArr[i].getBoundingClientRect().bottom;
-        //스크롤 올릴때
-        if(distinguishScrollUpDown == "Up"){
-            //효과 off
-            if(window.innerHeight- postWritesArrTop<50){
-                postWritesArr[i].style.webkitAnimationName = ""
-                postWriteShadow[i].style.webkitAnimationName = ""
-                postWritesArr[i].style.webkitAnimationDuration = ""
-                postWriteShadow[i].style.webkitAnimationDuration = ""
-                postWritesArr[i].style.opacity = 0;
-                // contentTireDrawFun("stop")
-                // toStopDrawFun[i] = 0;
-            }
-            // //효과 on
-            // else if(postWritesArrTop>15){
-                
-            //     postWritesArr[i].style.webkitAnimationName = "fadein"
-            //     postWritesArr[i].style.webkitAnimationDuration = "3s"
-            //     postWriteShadow[i].style.webkitAnimationName = "fadeinShadow"
-            //     postWriteShadow[i].style.webkitAnimationDuration = "3s"
-            //     postWritesArr[i].style.opacity = 1;
-            //     if(toStopDrawFun[i] ==1){
-            //         return
-            //     }
-            //     contentTireDrawFun();
-            //     toStopDrawFun[i] = 1;
-            // }   
-        }
-    
-        //스크롤 내릴때
-        else{
-            //효과 off
-            // if(postWritesArrBottom<150){
-            //     postWritesArr[i].style.webkitAnimationName = ""
-            //     postWriteShadow[i].style.webkitAnimationName = ""
-            //     postWritesArr[i].style.opacity = 0;
-            //     toStopDrawFun[i] = 0;
-            // }
-            // //효과 on
-            // else
-            if(window.innerHeight- postWritesArrBottom>-100){
-                return
-            }
-             if(window.innerHeight- postWritesArrTop>50){ 
-                postWritesArr[i].style.webkitAnimationName = "fadein"
-                postWritesArr[i].style.webkitAnimationDuration = "3s"
-                postWriteShadow[i].style.webkitAnimationName = "fadeinShadow"
-                postWriteShadow[i].style.webkitAnimationDuration = "3s"
-                postWritesArr[i].style.opacity = 1;
-                if(toStopDrawFun[i] ==1){
-                    return
-                }
-                // contentTireDrawFun();
-                // toStopDrawFun[i] = 1;
-            }
-            else{
-                postWritesArr[i].style.webkitAnimationName = ""
-                postWriteShadow[i].style.webkitAnimationName = ""
-                postWritesArr[i].style.webkitAnimationDuration = ""
-                postWriteShadow[i].style.webkitAnimationDuration = ""
-                postWritesArr[i].style.opacity = 0;
-            }
-        }
-    } 
-}
+  }
+};
