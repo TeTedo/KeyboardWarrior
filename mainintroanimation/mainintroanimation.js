36934859b350a115d081c8962847358ecdb4b31e
Taesk <qkrxotjr9508@gmail.com>
2022-07-11 22:49:22 +0900

    mainpage intro animation

--- mainintroanimation/mainintroanimation.js
+++ mainintroanimation/mainintroanimation.js
@@ -1,6 +1,8 @@
 let mainPageIntro = document.querySelector('.mainPageIntro')
 let block = document.querySelectorAll('.block')
-
+let mainPageIntroLogo = document.querySelector('.mainPageIntroLogo')
+mainPageIntroLogo.innerHTML = mainPageIntroLogo.textContent.replace(/\S/g,"<span>$&</span>")
+let smokeWords = mainPageIntroLogo.querySelectorAll('span')
 for(let i = 0; i <400; i++){
     let tempblock = document.createElement('div')
     tempblock.classList.add('block')
@@ -9,12 +11,20 @@ for(let i = 0; i <400; i++){
     tempblock.style.animationDuration = randNum + 's'
 }
 
+setTimeout(()=>{
+    for(let i = 0;i<smokeWords.length;i++){
+        let randNum = Math.random() * 3.5
+        smokeWords[i].style.animationName = "smokewords";
+        smokeWords[i].style.animationDuration = randNum + 's';
+        smokeWords[i].style.opacity = 0;
+    }
+},5500)
 
 setTimeout(()=>{
     mainPageIntro.style.animationName = "deletewords"
     mainPageIntro.style.animationDuration = "2s"
-},5500)
+},7500)
 
 setTimeout(()=>{
     mainPageIntro.style.display = "none"
-},7500)
+},8500)
