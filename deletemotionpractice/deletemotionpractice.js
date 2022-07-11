0bcba2f1dbace9881182ed2c8bea416993a53b5c
Taesk <qkrxotjr9508@gmail.com>
2022-07-11 18:29:34 +0900

    mainPageStarteffect

--- deletemotionpractice/deletemotionpractice.js
+++ deletemotionpractice/deletemotionpractice.js
@@ -1,10 +1,20 @@
-let Wrap = document.querySelector('.Wrap')
+let mainPageIntro = document.querySelector('.mainPageIntro')
 let block = document.querySelectorAll('.block')
 
 for(let i = 0; i <400; i++){
     let tempblock = document.createElement('div')
     tempblock.classList.add('block')
     let randNum = Math.random() * 5
-    Wrap.appendChild(tempblock)
+    mainPageIntro.appendChild(tempblock)
     tempblock.style.animationDuration = randNum + 's'
 }
+
+
+setTimeout(()=>{
+    mainPageIntro.style.animationName = "deletewords"
+    mainPageIntro.style.animationDuration = "2s"
+},5500)
+
+setTimeout(()=>{
+    mainPageIntro.style.display = "none"
+},7500)
