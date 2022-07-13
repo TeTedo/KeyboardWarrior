235a9ec0b0128e91f7933851f63c697cb56196f4
ssups <shalstlqruki@gmail.com>
2022-07-13 19:04:25 +0900

    벽돌부시기 미완

--- minigame/Games/BallBar/BallBar.js
+++ minigame/Games/BallBar/BallBar.js
@@ -5,34 +5,39 @@ function radian(angle) {
     return (angle * Math.PI) / 180;
 }
 
-const ballRadius = 10;
 const barWidth = 100;
-const barHeight = 15;
-// let ballStart = { x: ballRadius, y: ballRadius };
-let ballStart = { x: 300, y: ballRadius };
+const barHeight = 20;
+let barXMove = 0;
+let barYMove = 0;
+let barStart = { x: canvas.width / 2 - barWidth / 2, y: canvas.height - barHeight - 20 };
+let barXPos;
+
+const ballRadius = 10;
+let ballStart = { x: barStart.x + barWidth / 2, y: barStart.y - barHeight - 15 };
 let ballPos = { x: 0, y: 0 };
 let Xspeed = 3;
 let Yspeed = 3;
-let barXMove = 0;
-let barYMove = 0;
-
-let barStart = { x: canvas.width / 2 - barWidth / 2, y: canvas.height - barHeight - 20 };
-let barXPos;
 
 function ballDraw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     ballPos.x = ballStart.x += Xspeed;
     ballPos.y = ballStart.y += Yspeed;
+
+    // 벽에 부딛히면 튀기는 부분
     if (ballPos.x > canvas.width - ballRadius) {
         Xspeed -= Xspeed * 2;
     } else if (ballPos.x < ballRadius) {
         Xspeed += Xspeed * -2;
     }
-    // if (ballPos.y > canvas.height - ballRadius) {
-    //     Yspeed -= Yspeed * 2;
-    // } else if (ballPos.y < ballRadius) {
-    //     Yspeed += Yspeed * -2;
-    // }
+    // 막대에 부딛히면 튀기는 부분
+    if (
+        ballPos.y >= barStart.y - ballRadius &&
+        ballPos.x >= barStart.x + barXMove - ballRadius &&
+        ballPos.x <= barStart.x + barXMove + barWidth + ballRadius
+    ) {
+        Yspeed -= Yspeed * 2;
+    }
+    //바닦에 부딛히면 튀기는 부분
     if (ballPos.y > canvas.height - ballRadius) {
         Yspeed -= Yspeed * 2;
     } else if (ballPos.y < ballRadius) {
@@ -58,6 +63,24 @@ function ballDraw() {
 }
 
 ballDraw();
+
+//벽돌 그리는 부분
+const brickWidth = 100;
+const brickHeight = 20;
+const bricksBundleHeight = (canvas.height * 2) / 5;
+let bricksCol = 0;
+let bricksRow = 0;
+
+function bricksDraw() {
+    for (let i = 0; i < bricksBundleHeight / brickHeight; i++) {
+        for (let k = 0; k < canvas.width / brickWidth; k++) {
+            console.log('ok');
+            context.strokeRect(k * brickWidth, i * brickHeight, brickWidth, brickHeight);
+        }
+    }
+    requestAnimationFrame(bricksDraw);
+}
+bricksDraw();
 
 window.onkeydown = function (e) {
     barXpos = barStart.x + barXMove;
