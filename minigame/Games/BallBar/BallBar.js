0eb0ed6304d6b00759da463a1617b40c9591b197
ssups <shalstlqruki@gmail.com>
2022-07-13 16:58:48 +0900

    게임추가중

--- minigame/Games/BallBar/BallBar.js
+++ minigame/Games/BallBar/BallBar.js
@@ -13,9 +13,11 @@ let ballStart = { x: 300, y: ballRadius };
 let ballPos = { x: 0, y: 0 };
 let Xspeed = 3;
 let Yspeed = 3;
+let barXMove = 0;
+let barYMove = 0;
 
 let barStart = { x: canvas.width / 2 - barWidth / 2, y: canvas.height - barHeight - 20 };
-console.log(barStart.x);
+let barXPos;
 
 function ballDraw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -26,6 +28,11 @@ function ballDraw() {
     } else if (ballPos.x < ballRadius) {
         Xspeed += Xspeed * -2;
     }
+    // if (ballPos.y > canvas.height - ballRadius) {
+    //     Yspeed -= Yspeed * 2;
+    // } else if (ballPos.y < ballRadius) {
+    //     Yspeed += Yspeed * -2;
+    // }
     if (ballPos.y > canvas.height - ballRadius) {
         Yspeed -= Yspeed * 2;
     } else if (ballPos.y < ballRadius) {
@@ -45,7 +52,7 @@ function ballDraw() {
     context.fill();
 
     // 막대 그리는 부분
-    context.fillRect(barStart.x, barStart.y, barWidth, barHeight);
+    context.fillRect(barStart.x + barXMove, barStart.y + barYMove, barWidth, barHeight);
 
     requestAnimationFrame(ballDraw);
 }
@@ -53,9 +60,14 @@ function ballDraw() {
 ballDraw();
 
 window.onkeydown = function (e) {
+    barXpos = barStart.x + barXMove;
     switch (e.keyCode) {
         case 37:
             console.log('왼쪽키눌림');
+            if (barXpos <= 0) {
+                return;
+            }
+            barXMove -= 20;
             break;
 
         case 38:
@@ -63,6 +75,10 @@ window.onkeydown = function (e) {
             break;
         case 39:
             console.log('오른쪽키눌림');
+            if (barXpos >= canvas.width - barWidth) {
+                return;
+            }
+            barXMove += 20;
             break;
         case 40:
             console.log('아래쪽키눌림');
