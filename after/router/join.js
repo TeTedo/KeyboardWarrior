8a633129ea2cebd0711aa4ae896eb26984b1ef6a
ssups <shalstlqruki@gmail.com>
2022-08-21 23:36:50 +0900

    아이디 및 닉네임 중복검사할 방법 고민중..

--- after/router/join.js
+++ after/router/join.js
@@ -4,12 +4,16 @@ const User = require("../model/users");
 const bcrypt = require("bcrypt");
 
 router.get("/join", (req, res) => {
-    res.render("join/join");
+    // res.render("join/join");
+    User.findAll({}).then(allData => {
+        const userIds = allData.map(eachData => eachData.user_id);
+        res.render("join/join", { userIds });
+        console.log(userIds);
+    });
 });
 router.post("/join", (req, res) => {
     const { user_id, user_pw, name, nick_name, mobile_number, email } =
         req.body;
-    console.log(mobile_number);
     // bcrypt 활용 비밀번호 암호화
     bcrypt.hash(user_pw, 10, (err, encrypted) => {
         User.create({
