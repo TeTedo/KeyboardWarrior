5637ca37ad6d41d8c8807d1ab50cff0730b02cf7
ssups <shalstlqruki@gmail.com>
2022-08-21 21:23:15 +0900

    git ignore 했고, 폰번호컬럼 정수형으로  해놓으니깐 앞에 젤첫자리 0 없애버려서 문자형으로 바꿈

--- after/router/join.js
+++ after/router/join.js
@@ -9,6 +9,7 @@ router.get("/join", (req, res) => {
 router.post("/join", (req, res) => {
     const { user_id, user_pw, name, nick_name, mobile_number, email } =
         req.body;
+    console.log(mobile_number);
     // bcrypt 활용 비밀번호 암호화
     bcrypt.hash(user_pw, 10, (err, encrypted) => {
         User.create({
