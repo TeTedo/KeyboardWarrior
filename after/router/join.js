7b4aa436fd537c7745bd36440fc6c88756ec58e8
ssups <shalstlqruki@gmail.com>
2022-08-21 17:37:33 +0900

    password encrypt

--- after/router/join.js
+++ after/router/join.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../model/users");
+const bcrypt = require("bcrypt");
 
 router.get("/join", (req, res) => {
     res.render("join/join");
@@ -8,15 +9,17 @@ router.get("/join", (req, res) => {
 router.post("/join", (req, res) => {
     const { user_id, user_pw, name, nick_name, mobile_number, email } =
         req.body;
-    User.create({
-        user_id,
-        user_pw,
-        name,
-        nick_name,
-        mobile_number,
-        email,
+    // bcrypt 활용 비밀번호 암호화
+    bcrypt.hash(user_pw, 10, (err, encrypted) => {
+        User.create({
+            user_id,
+            user_pw: encrypted,
+            name,
+            nick_name,
+            mobile_number,
+            email,
+        });
     });
-    res.redirect("/login");
 });
 
 module.exports = router;
