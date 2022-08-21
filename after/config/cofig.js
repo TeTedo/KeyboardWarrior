3b795d3193322f1ef27008431ed437d430206e01
Taesk <qkrxotjr9508@gmail.com>
2022-08-21 20:15:12 +0900

    nodemodule ignore

--- after/config/cofig.js
+++ after/config/cofig.js
@@ -1,11 +1,11 @@
 const dot = require("dotenv").config();
 const config = {
-    dev: {
-        username: "root",
-        password: process.env.DB_PASSWORD,
-        database: "keboard_warrior",
-        host: "localhost",
-        dialect: "mysql",
-    },
+  dev: {
+    username: "root",
+    password: process.env.DB_PASSWORD,
+    database: "keboard_warrior",
+    host: "localhost",
+    dialect: "mysql",
+  },
 };
 module.exports = config;
--- after/functions/pwFunctions.js
+++ after/functions/pwFunctions.js
@@ -3,27 +3,27 @@ const crypto = require("crypto");
 
 // salt값 생성 함수
 function createSalt() {
-    return new Promise((resolve, reject) => {
-        crypto.randomBytes(64, (err, buf) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(buf.toString("base64"));
-            }
-        });
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(64, (err, buf) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buf.toString("base64"));
+      }
     });
+  });
 }
 
 // 해싱+암호화 결합한 비밀번호 생성 함수(비밀번호랑 솔트값 붙여서 같이해싱해버림)
 function CreateHashedPw(password) {
-    return new Promise(async (resolve, reject) => {
-        const salt = await createSalt();
-        crypto.pbkdf2(password, salt, 1000, 64, "sha512");
-    });
+  return new Promise(async (resolve, reject) => {
+    const salt = await createSalt();
+    crypto.pbkdf2(password, salt, 1000, 64, "sha512");
+  });
 }
 
 const pwFunctions = {
-    CreateHashedPw,
+  CreateHashedPw,
 };
 
 module.exports = pwFunctions;
