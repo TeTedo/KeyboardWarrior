0afa7fa3b81fbf12897151a83bd1e59db16d9324
Taesk <qkrxotjr9508@gmail.com>
2022-08-22 18:49:15 +0900

    8.22

--- after/views/main/main.js
+++ after/views/main/main.js
@@ -1,2 +1,13 @@
 // scroll evnet
-console.log("hi");
+let scrollx = 0;
+let mouseMove = true;
+const mainContent = document.querySelector("#mainContent");
+// 가로로 스크롤 이동
+mainContent.onscroll = function (e) {
+  console.log(mainContent.style.top);
+  scrollx += 10;
+  console.log(mainContent.scrollY);
+  console.log(window.scrollY);
+  //   mainContent.scrollTo(scrollx, 3);
+  console.log("이동");
+};
