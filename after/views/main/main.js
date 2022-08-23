ca57f523a03c041d4eab728687e9a59a5bf3bff2
Taesk <qkrxotjr9508@gmail.com>
2022-08-23 17:25:11 +0900

    modify main.html

--- after/views/main/main.js
+++ after/views/main/main.js
@@ -1,12 +1,10 @@
 // scroll evnet
 let scrollx = 0;
-let mouseMove = true;
-const mainContent = document.querySelector("#mainContent");
-
 // 가로로 스크롤 이동
 mainContent.onwheel = function (e) {
-  const mainPost = document.querySelectorAll("#mainPost");
-  const maxScroll = (mainPost.length - 3) * 330;
+  const mainContent = document.querySelector("#mainContent");
+  const mainContentWrap = document.querySelector("#mainContentWrap");
+  const maxScroll = mainContentWrap.offsetWidth - mainContent.offsetWidth;
   scrollx = scrollx < 0 ? 0 : scrollx > maxScroll ? maxScroll : scrollx;
   //아래로 스크롤인 경우
   if (e.deltaY > 0) {
