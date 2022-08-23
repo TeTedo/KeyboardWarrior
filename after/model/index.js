077cdcaefab6d51a93fac2820af552aa5f1519e3
Taesk <qkrxotjr9508@gmail.com>
2022-08-23 12:38:06 +0900

    test

--- after/model/index.js
+++ after/model/index.js
@@ -1,18 +1,18 @@
 const Sequelize = require("sequelize");
 const config = require("../config/cofig");
 const User = require("./users");
-
+const Login = require("./login");
 const sequelize = new Sequelize(
-    config.dev.database,
-    config.dev.username,
-    config.dev.password,
-    config.dev
+  config.dev.database,
+  config.dev.username,
+  config.dev.password,
+  config.dev
 );
 
 const db = {};
 db.sequelize = sequelize;
 db.User = User;
-
+db.Login = Login;
 User.init(sequelize);
 
 module.exports = db;
