12638df0edc0599ef4e0a0ba84ae4482864b0008
Taesk <qkrxotjr9508@gmail.com>
2022-08-24 18:52:03 +0900

    8.24

--- after/router/logout.js
+++ after/router/logout.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { Token } = require("../model");
-const jwt = require("jsonwebtoken");
-const dot = require("dotenv");
-dot.config();
+
 const getUserId = require("../functions/getUserId");
 router.get("/logout", (req, res) => {
   const user_id = getUserId(req, res);
-  console.log(user_id, "는 로그아웃 됨");
-  res.redirect("/");
+  Token.destroy({
+    where: { user_id },
+  }).then(() => {
+    req.session.access_token = null;
+    req.session.refresh_token = null;
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
