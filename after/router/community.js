11c52676c02dadc11bf97ccb927ce0d2e229b97c
ssups <shalstlqruki@gmail.com>
2022-08-24 16:51:37 +0900

    communitypage

--- after/router/community.js
+++ after/router/community.js
@@ -3,14 +3,35 @@ const router = express.Router();
 
 router.get("/community/:game_name", (req, res) => {
     const gameName = req.params.game_name;
-    switch (game_name) {
-        case value:
-            break;
+    let data = {};
+    res.render("community/community", { gameName });
 
-        default:
-            break;
-    }
-    res.render("community/community", { gameName, title });
+    // switch (gameName) {
+    //     case "fifa":
+    //         data = {
+    //             gameName,
+    //             title: "피파온라인 4",
+    //         };
+    //         res.render("community/community", { data });
+    //         break;
+    //     case "maple":
+    //         data = {
+    //             gameName,
+    //             title: "메이플스토리",
+    //         };
+    //         res.render("community/community", { data });
+    //         break;
+    //     case "riniji":
+    //         data = {
+    //             gameName,
+    //             title: "리니지",
+    //         };
+    //         res.render("community/community", { data });
+    //         break;
+    //     default:
+    //         res.redirect("community_hub/community_hub");
+    //         break;
+    // }
 });
 
 module.exports = router;
