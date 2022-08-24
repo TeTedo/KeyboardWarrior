58f2e9bccfbbc1eac3e442895834977e87fe5a27
Taesk <qkrxotjr9508@gmail.com>
2022-08-25 00:57:29 +0900

    메인 로드 에러 잡기

--- after/router/logout.js
+++ after/router/logout.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { Token } = require("../model");
 
-const getUserId = require("../functions/getUserId");
 router.get("/logout", (req, res) => {
-  const user_id = getUserId(req, res);
-  Token.destroy({
-    where: { user_id },
-  }).then(() => {
+  if (req.session?.access_token) {
     req.session.access_token = null;
     req.session.refresh_token = null;
     res.redirect("/");
-  });
+  } else {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
