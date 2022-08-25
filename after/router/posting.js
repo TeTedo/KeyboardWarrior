e9c6fa5bb5755aa2b4fe6b523991819b7eaff332
Taesk <qkrxotjr9508@gmail.com>
2022-08-25 17:34:45 +0900

    img base64

--- after/router/posting.js
+++ after/router/posting.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const loginCheck = require("../middleware/loginCheck");
+
 router.get("/posting", loginCheck, (req, res) => {
   res.render("posting/posting");
+});
+
+router.post("/posting", loginCheck, (req, res) => {
+  const { img, text } = req.body;
+  console.log(img, text);
 });
 
 module.exports = router;
--- after/views/posting/posting.js
+++ after/views/posting/posting.js
@@ -1,13 +1,60 @@
 //readURL function
-function readURL(input) {
+function readURL(input, callback) {
   if (input.files[0]) {
     const reader = new FileReader();
+
     //파일 읽기 성공했을때마다 실행
     reader.onload = function (e) {
+      //큰 화면에 이미지 띄우기
       document.querySelector("#posting_left > img").src = e.target.result;
+
+      //미리보기에 태그 추가
+      const preImg = document.querySelector("#posting_right_imagePreview");
+      preImg.innerHTML += `
+      <img src = ${e.target.result}>
+      `;
+      callback();
     };
+
+    // file 읽어오기
     reader.readAsDataURL(input.files[0]);
   } else {
     document.querySelector("#posting_left > img").src = "";
   }
 }
+
+function clickPreImg() {
+  const imgPreview = document.querySelectorAll(
+    "#posting_right_imagePreview > img"
+  );
+
+  imgPreview.forEach((el) => {
+    el.onclick = function () {
+      document.querySelector("#posting_left > img").src = el.src;
+
+      // el.src는 (data:*/*;base64, base64로 인코딩된 코드) 형식으로 되있다.
+      imgData.push(el.src);
+    };
+  });
+}
+
+//함수 실행 부분
+document.querySelector("input[type=file]").onchange = function () {
+  readURL(this, clickPreImg);
+};
+
+//데이터 베이스에 보내기
+const imgData = [];
+document.querySelector("#sumbit").onclick = function () {
+  console.log(imgData);
+  console.log("확인");
+  const textData = document.querySelector("#posting_right > textarea").value;
+  $.ajax({
+    type: "post",
+    url: "/posting",
+    data: {
+      img: imgData,
+      text: textData,
+    },
+  });
+};
