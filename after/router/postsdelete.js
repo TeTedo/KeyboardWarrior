2c23ad698d354f4f0e9f4dfcaf4dc75b3c0a5fe3
Taesk <qkrxotjr9508@gmail.com>
2022-08-30 23:46:29 +0900

    사이드바 열기 닫기

--- after/router/postsdelete.js
+++ after/router/postsdelete.js
@@ -3,23 +3,6 @@ const router = express.Router();
 const getUserInfo = require("../functions/getUserInfo");
 const { MainPost } = require("../model");
 
-router.get("/posts/:postId/delete", async (req, res) => {
-  const postId = req.params.postId;
-  console.log(req.body);
-
-  const postData = await MainPost.findOne({
-    where: { id: postId },
-    raw: true,
-  });
-
-  const { user_id, nick_name } = await getUserInfo(req, res);
-
-  // 로그인 유저와 글 작성자 비교
-  if (postData.user_id !== user_id) {
-    res.redirect("/");
-  }
-});
-
 router.post("/posts/:postId/delete", async (req, res) => {
   const postId = req.params.postId;
   const { user_id } = await getUserInfo(req, res);
--- after/views/posts/posts.js
+++ after/views/posts/posts.js
@@ -4,3 +4,15 @@ document.querySelector(
 ).onclick = function () {
   document.querySelector(".posts_comment").classList.toggle("toggle_show");
 };
+
+// 사이드바 접기
+const closeSideBar = document.querySelector(".fa-arrow-left-long");
+const openSideBar = document.querySelector(".fa-arrow-right-long");
+const sideBar = document.querySelector("#sideBar");
+closeSideBar.onclick = function () {
+  sideBar.style.transform = "translate(-450px)";
+  openSideBar.style.display = "block";
+};
+openSideBar.onclick = function () {
+  sideBar.style.transform = "translate(0)";
+};
