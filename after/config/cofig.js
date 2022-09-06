d8af55716b5e38d91f8c37d921a8c64d4687e53c
Taesk <qkrxotjr9508@gmail.com>
2022-09-06 12:27:39 +0900

    시간 통일, 표시

--- after/config/cofig.js
+++ after/config/cofig.js
@@ -8,6 +8,14 @@ const config = {
     database: "keboard_warrior",
     host: "localhost",
     dialect: "mysql",
+    timezone: "+09:00",
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true, // DB에서 가져올 때 시간 설정
+    },
+    define: {
+      timestamps: true,
+    },
   },
 };
 
