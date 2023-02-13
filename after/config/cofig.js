665d08d917c9b3c75e3c564e3022f3f5ccf0b09e
TeTedo <qkrxotjr9508@gmail.com>
2023-02-13 12:13:20 +0900

    test

--- after/config/cofig.js
+++ after/config/cofig.js
@@ -4,18 +4,18 @@ const dot = require("dotenv").config();
 const config = {
   dev: {
     username: "keboard_warrior",
-    password: process.env.DB_PASSWORD,
+    password: "keboard_warrior",
     database: "keboard_warrior",
     host: "localhost",
     dialect: "mysql",
     timezone: "+09:00",
+    operatorsAliases: 0,
+    logging: false,
     dialectOptions: {
       dateStrings: true,
       typeCast: true, // DB에서 가져올 때 시간 설정
     },
-    define: {
-      timestamps: true,
-    },
+    port: 3306,
   },
   prod: {
     username: "keboard_warrior",
