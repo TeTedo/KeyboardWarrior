1f4da9b63cf8aab0abd2d4c925eaab25ace25c4f
TeTedo <qkrxotjr9508@gmail.com>
2023-02-13 12:07:41 +0900

    test

--- after/config/cofig.js
+++ after/config/cofig.js
@@ -3,7 +3,7 @@ const { sequelize } = require("../model");
 const dot = require("dotenv").config();
 const config = {
   dev: {
-    username: "keboard_warrio",
+    username: "keboard_warrior",
     password: process.env.DB_PASSWORD,
     database: "keboard_warrior",
     host: "localhost",
